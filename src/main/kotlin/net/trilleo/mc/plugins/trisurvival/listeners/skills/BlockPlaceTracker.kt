e8fee48af0145fa6441d84c8465783e8adafb81d
Trilleo <gongsanle@gmail.com>
2026-06-11 16:32:51 +0800

    Improvement: Reformat source code

--- src/main/kotlin/net/trilleo/mc/plugins/trisurvival/listeners/skills/BlockPlaceTracker.kt
+++ src/main/kotlin/net/trilleo/mc/plugins/trisurvival/listeners/skills/BlockPlaceTracker.kt
@@ -6,11 +6,7 @@ import org.bukkit.block.BlockFace
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
-import org.bukkit.event.block.BlockBreakEvent
-import org.bukkit.event.block.BlockExplodeEvent
-import org.bukkit.event.block.BlockPistonExtendEvent
-import org.bukkit.event.block.BlockPistonRetractEvent
-import org.bukkit.event.block.BlockPlaceEvent
+import org.bukkit.event.block.*
 import org.bukkit.event.entity.EntityChangeBlockEvent
 import org.bukkit.event.entity.EntityExplodeEvent
 import org.bukkit.persistence.PersistentDataType
